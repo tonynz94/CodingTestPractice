a6b18d2c401b68291ab9c9945a031de72a06b5d1
tonynz94 <tonynz94@naver.com>
2022-02-03 22:27:25 +0900

    문제풀이

---
+++ CodingAlgoTest/60_합이같은부분집합/60_합이같은부분집합.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+int n;
+int s[11];
+bool isTrue = false;
+
+void DFS(int level, int ASum, int BSum)
+{
+    //이미 넘어 오던게 있었다면.
+    if (isTrue)
+        return;
+
+    if (level == n + 1)
+    {
+        if (ASum == BSum) {
+            std::cout << ASum << " : " << BSum << std::endl;
+            isTrue = true;
+        }
+        return;
+    }
+
+    DFS(level + 1, ASum + s[level], BSum);
+    DFS(level + 1, ASum, BSum + s[level]);
+
+}
+
+
+int main()
+{
+    std::cin >> n;
+    int i = 0;
+    for (i = 1; i <= n; i++)
+    {
+        std::cin >> s[i];
+    }
+    DFS(1, 0, 0);
+
+    if (isTrue)
+        std::cout << "YES" << std::endl;
+    else
+        std::cout << "NO" << std::endl;
+}
---
+++ CodingAlgoTest/61_특정수만들기/61_특정수만들기.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+
+int s[11];
+int sum;
+int n;
+int cnt = 0;
+
+void DFS(int level, int m)
+{
+	if (level == n + 1)
+	{
+		if (m == sum)
+			cnt++;
+		return;
+	}
+
+	DFS(level + 1, m);
+	DFS(level + 1, m + s[level]);
+	DFS(level + 1, m - s[level]);
+
+
+
+}
+
+
+int main()
+{
+	cin >> n;
+	cin >> sum;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> s[i];
+	}
+
+	DFS(1,0);
+
+	if(cnt > 0)
+		cout << cnt << endl;
+	else
+		cout << -1 << endl;
+}
