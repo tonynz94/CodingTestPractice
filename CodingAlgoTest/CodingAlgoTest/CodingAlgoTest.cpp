d2c06aa998d678dc076e574352a3b531b3bc5b94
tonynz94 <49948106+tonynz94@users.noreply.github.com>
2022-02-21 23:56:47 +0900

    경로탐색 가중치 인접리스트

--- CodingAlgoTest/CodingAlgoTest/CodingAlgoTest.cpp
+++ CodingAlgoTest/CodingAlgoTest/CodingAlgoTest.cpp
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿#include <iostream>
 
 
@@ -31,3 +32,45 @@ void BFS(int level)
         std::cout << level << std::endl; //후위
     }
 }
+=======
+﻿#include<stdio.h>
+#include<vector>
+using namespace std;
+int ch[30], cnt = 0, n, path[30];
+vector<int> map[30];
+void DFS(int v, int L) {
+	int i, j;
+	if (v == n) {
+		cnt++;
+		for (j = 0; j < L; j++) {
+			printf("%d ", path[j]);
+		}
+		puts("");
+	}
+	else {
+		for (i = 0; i < map[v].size(); i++) {
+			if (ch[map[v][i]] == 0) {
+				ch[map[v][i]] = 1;
+				path[L] = map[v][i];
+				DFS(map[v][i], L + 1);
+				ch[map[v][i]] = 0;
+			}
+		}
+	}
+}
+
+int main() {
+	freopen("input.txt", "rt", stdin);
+	int m, i, j, a, b, c;
+	scanf("%d %d", &n, &m);
+	for (i = 1; i <= m; i++) {
+		scanf("%d %d", &a, &b);
+		map[a].push_back(b);
+	}
+	ch[1] = 1;
+	path[0] = 1;
+	DFS(1, 1);
+	printf("%d\n", cnt);
+	return 0;
+}
+>>>>>>> Stashed changes
