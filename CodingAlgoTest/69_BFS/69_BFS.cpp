c4da5098a33b8743e98e933478f49913fa46af29
tonynz94 <49948106+tonynz94@users.noreply.github.com>
2022-02-26 23:56:48 +0900

    70_BFS_그래프_목적지까지의_모든경로_출력

--- CodingAlgoTest/69_BFS/69_BFS.cpp
+++ CodingAlgoTest/69_BFS/69_BFS.cpp
@@ -4,27 +4,41 @@
 이진트리 넓이우선탐색(BFS)
 아래 그림과 같은 이진트리를 넓이우선탐색해 보세요. 간선 정보 6개를 입력받아 처리해보세
 요.
-1
-2 3
-4 5 6 7
+       1
+      2 3
+	4 5 6 7
 넓이 우선 탐색 : 1 2 3 4 5 6 7
+
+입력
+1 2
+1 3
+2 4
+2 5
+3 6
+3 7
+
 */
 
 //
 
 #include<stdio.h>
+#include <iostream>
 #include<vector>
 #include<algorithm>
 using namespace std;
 int Q[100], front = -1, back = -1, ch[10];
 vector<int> map[10];
+
+vector<pair<int, int>> v[10];
 int main() {
 	int i, a, b, x;
 	for (i = 1; i <= 6; i++) {
-		scanf("%d %d", &a, &b);
+		cin >> a;
+		cin >> b;
 		map[a].push_back(b);
 		map[b].push_back(a);
 	}
+	v[2].push_back(make_pair(10, 20));
 	Q[++back] = 1;
 	ch[1] = 1;
 	while (front < back) {
---
+++ CodingAlgoTest/70_BFS_그래프_목적지까지의_모든경로_출력/70_BFS_그래프_목적지까지의_모든경로_출력.cpp
@@ -0,0 +1,82 @@
+﻿// 70_BFS_그래프_목적지까지의_모든경로_출력.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+/*
+70. 그래프 최단거리(BFS)
+다음 그래프에서 1번 정점에서 각 정점으로 가는 최소 이동 간선수를 출력하세요.
+(pdf 이미지 참조)
+▣ 입력설명
+첫째 줄에는 정점의 수 N(1<=N<=20)와 간선의 수 M가 주어진다. 그 다음부터 M줄에 걸쳐 연
+결정보가 주어진다.
+▣ 출력설명
+1번 정점에서 각 정점으로 가는 최소 간선수를 2번 정점부터 차례대로 출력하세요.
+
+-방향 그래프-
+▣ 입력예제 1
+`   6 9	 (정점수, 간선 수)
+`   1 3 (정점 -> 정점)
+`   1 4
+`   2 1
+`   2 5
+`   3 4
+`   4 5
+`   4 6
+`   6 2
+`   6 5
+▣ 출력예제 1
+`   2 : 3
+`   3 : 1
+`   4 : 1
+`   5 : 2
+`   6 : 2
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int back = -1;
+	int front = -1;
+
+	int N, M,a,b;
+	int arr[10];
+	int answer[10] = { 0 };
+	int num = 1;
+	int ch[10] = {0};
+	cin >> N;	//정점 수
+	cin >> M;	//간선 수
+
+	vector<int> map[10];
+
+	//인접 리스트
+	for (int i = 1; i <= M; i++)
+	{
+		cin >> a;
+		cin >> b;
+		map[a].push_back(b);
+	}
+
+	ch[1] = 1;
+	arr[++back] = 1;
+	while (back > front)
+	{
+		
+		int temp = arr[++front];
+
+		for (int i = 0; i < map[temp].size(); i++)
+		{
+			if (ch[map[temp][i]] == 0)
+			{
+				ch[map[temp][i]] = num;
+				arr[++back] = map[temp][i];
+			}		
+		}
+		num++;
+	}
+
+	for (int i = 1; i <= N; i++)
+	{
+		cout << i +" + " + ch[i] << endl;
+	}
+}
