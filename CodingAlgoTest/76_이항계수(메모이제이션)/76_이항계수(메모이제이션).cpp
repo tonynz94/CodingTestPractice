2050a200dd1cab75c60ffc9c1ed1e3844391f915
tonynz94 <49948106+tonynz94@users.noreply.github.com>
2022-03-04 02:35:08 +0900

    메모이제이션

--- CodingAlgoTest/76_이항계수(메모이제이션)/76_이항계수(메모이제이션).cpp
+++ CodingAlgoTest/76_이항계수(메모이제이션)/76_이항계수(메모이제이션).cpp
@@ -1,8 +1,7 @@
 ﻿// 76_이항계수(메모이제이션).cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 /*
 이항계수는 N개의 원소를 가지는 집합에서 R개의 원소를 뽑아 부분집합을 만드는 경우의 수
-를 의미한다. 공식은 
- 로 표현된다.
+를 의미한다. 5C3
 N과 R이 주어지면 이항계수를 구하는 프로그램을 작성하세요.
 ▣ 입력설명
 첫 번째 줄에 자연수 N(1<=N<=20)과 R(0<=R<=20)이 주어진다. 단 (N>=R)
@@ -16,18 +15,23 @@ N과 R이 주어지면 이항계수를 구하는 프로그램을 작성하세요
 
 #include <iostream>
 using namespace std;
-
-int dy[21][21];
-
-int C(int n, int r) {
-	if (dy[n][r] > 0) return dy[n][r];
-	if (n == r || r == 0) return 1;
-	else return dy[n][r] = C(n - 1, r) + C(n - 1, r - 1);
+int mem[20][20];
+int C(int n, int r)
+{
+    if(mem[n][r] != 0)
+        return mem[n][r];
+    if (n == r || r == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return mem[n][r] = C(n - 1, r - 1) + C(n - 1, r);
+    }
 }
 
 int main() {
-	int n, r;
-	cin >> n >> r;
-	cout << C(n, r);
-	return 0;
+    int n, r;
+    cin >> n >> r;
+    cout << C(n, r);
 }
